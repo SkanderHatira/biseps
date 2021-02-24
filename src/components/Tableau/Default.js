18cb89e9f79ff2814b0abac57ad0fa4e7b758064
Skander Hatira <skander.hatira@inrae.fr>
2021-02-24 21:10:52 +0100

    Added DashLayout and working on different views for reports and runs

---
+++ src/components/Tableau/Default.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Chart from "./Chart";
+import Deposits from "./Deposits";
+import Orders from "./Orders";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+
+const Default = ({ Copyright, classes, fixedHeightPaper }) => {
+  return (
+    <Container maxWidth="lg" className={classes.container}>
+      <Grid container spacing={3}>
+        {/* Chart */}
+        <Grid item xs={12} md={8} lg={9}>
+          <Paper className={fixedHeightPaper}>
+            <Chart />
+          </Paper>
+        </Grid>
+        {/* Recent Deposits */}
+        <Grid item xs={12} md={4} lg={3}>
+          <Paper className={fixedHeightPaper}>
+            <Deposits />
+          </Paper>
+        </Grid>
+        {/* Recent Orders */}
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Orders />
+          </Paper>
+        </Grid>
+      </Grid>
+      <Box pt={4}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+};
+
+export default Default;
