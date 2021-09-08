af6bbbdfdbbad28e49c7eeacbbb59d087f727b91
Skander Hatira <skander.hatira@inrae.fr>
2021-09-08 13:08:46 +0200

    execSync for generating assets

--- hooks.js
+++ hooks.js
@@ -1,12 +1,12 @@
 // ./hooks.js
 
 const path = require("path");
-const { exec } = require("child_process");
+const { execSync } = require("child_process");
 const fs = require("fs");
 
 module.exports = {
   generateAssets: async (forgeConfig, options) => {
-    exec(
+    execSync(
       `jbrowse create ${__dirname}/resources/jbrowse2 -f || true`,
       (error, stdout, stderr) => {
         if (error) {
@@ -38,28 +38,5 @@ module.exports = {
     // });
   },
 
-  postPackage: async (forgeConfig, options) => {
-    // const resources = path.join(
-    //   options.outputPaths[0],
-    //   "resources/app/.webpack/main/resources"
-    // );
-    // const jbrowse = path.join(
-    //   options.outputPaths[0],
-    //   "resources/app/.webpack/main/backend/node_modules/"
-    // );
-    // chmodr(resources, 0o777, (err) => {
-    //   if (err) {
-    //     console.log("Failed to execute chmod", err);
-    //   } else {
-    //     console.log("Successful");
-    //   }
-    // });
-    // chmodr(jbrowse, 0o777, (err) => {
-    //   if (err) {
-    //     console.log("Failed to execute chmod", err);
-    //   } else {
-    //     console.log("Successful");
-    //   }
-    // });
-  },
+  postPackage: async (forgeConfig, options) => {},
 };
