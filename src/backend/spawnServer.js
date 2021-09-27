bc56f5051cb4bc80081ac231b665595a58b054a6
Skander Hatira <skander.hatira@inrae.fr>
2021-09-27 11:43:49 +0200

    cleanup

--- src/backend/spawnServer.js
+++ src/backend/spawnServer.js
@@ -2,28 +2,7 @@ const spawnServer = async (sock) => {
     const { fork, exec } = require("child_process");
     const path = require("path");
     const fs = require("fs");
-    // exec(
-    //     "bash  " + path.join(__dirname, "resources/checkConda.sh"),
-    //     (error, stdout, stderr) => {
-    //         fs.writeFileSync(
-    //             "/home/shatira/server.txt",
-    //             stdout + error + stderr,
-    //             function (err) {
-    //                 if (err) throw err;
-    //                 console.log("Saved!");
-    //             }
-    //         );
 
-    //         if (error) {
-    //             console.log(`error: ${error.message}`);
-    //         }
-    //         if (stderr) {
-    //             console.log(`stderr: ${stderr}`);
-    //         }
-
-    //         console.log(`stdout: ${stdout}`);
-    //     }
-    // );
     const options = {
         slient: false,
         detached: true,
