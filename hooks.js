8e1776aa9668959f8155560e9ec9f27481941f8b
Skander Hatira <skander.hatira@inrae.fr>
2022-04-14 16:47:03 +0200

    try fix

--- hooks.js
+++ hooks.js
@@ -44,7 +44,6 @@ module.exports = {
     }
 
     if (!fs.existsSync(pipeline)) {
-      console.log(process.env.ACCESS_TOKEN);
       execSync(
         `git clone https://o2auth:${process.env.ACCESS_TOKEN}@forgemia.inra.fr/skander.hatira/biseps.git ${pipeline}`,
         (error, stdout, stderr) => {}
@@ -53,7 +52,43 @@ module.exports = {
       console.log("Pipeline is already installed");
     }
   },
-  postStart: async (forgeConfig, options) => {},
+  postStart: async (forgeConfig, options) => {
+    fs.access(path.join(resources, "jbrowse2"), function (error) {
+      if (error) {
+        execSync(
+          `npx  @jbrowse/cli  create ${path.join(resources, "jbrowse2")} -f`,
+          (error, stdout, stderr) => {
+            if (error) {
+              console.log(`error: ${error.message}`);
+              return;
+            }
+            if (stderr) {
+              console.log(`stderr: ${stderr}`);
+              return;
+            }
+            console.log(`stdout: ${stdout}`);
+          }
+        );
+      } else {
+        console.log("Jbrowse present, moving on...");
+      }
+    });
+    if (!fs.existsSync(bisepsConfigFile)) {
+      fs.mkdirSync(bisepsHidden, { recursive: true });
+      fs.writeFileSync(bisepsConfigFile, jsonContent);
+    } else {
+      console.log("Config file already exists, moving on ...");
+    }
+
+    if (!fs.existsSync(pipeline)) {
+      execSync(
+        `git clone https://o2auth:${process.env.ACCESS_TOKEN}@forgemia.inra.fr/skander.hatira/biseps.git ${pipeline}`,
+        (error, stdout, stderr) => {}
+      );
+    } else {
+      console.log("Pipeline is already installed");
+    }
+  },
 
   postPackage: async (forgeConfig, options) => {},
 };
